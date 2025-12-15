e60c99e8049838f5f02b6f0e8df4abf4b78d632a
Adrian <Adrian1568@live.com>
2025-12-14 21:38:50 -0500

    Update models.py

--- first web/pythonwebsite/pyfiles/models.py
+++ first web/pythonwebsite/pyfiles/models.py
@@ -1,21 +1,23 @@
+# imports database, usermixin, function
 from . import db
 from flask_login import UserMixin
 from sqlalchemy.sql import func
-
+# the note model for notes created by users 
 class Note(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    data = db.Column(db.String(10000))
-    date = db.Column(db.DateTime(timezone=True), default=func.now())
+    id = db.Column(db.Integer, primary_key=True) # id for each note created
+    data = db.Column(db.String(10000)) # notes can have up to 10,000 characters 
+    date = db.Column(db.DateTime(timezone=True), default=func.now()) # date and time note was made
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
-
+# the user model for registered users
 class User(db.Model, UserMixin):
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(150), unique=True)
-    password = db.Column(db.String(150))
-    first_name = db.Column(db.String(150))
+    id = db.Column(db.Integer, primary_key=True) # user id
+    email = db.Column(db.String(150), unique=True) # user email
+    password = db.Column(db.String(150)) # user hashed password
+    first_name = db.Column(db.String(150)) # user's first name
     # Add more fields if necessary
     notes = db.relationship('Note')
     # a relationship to the Note class model
+
 
 
