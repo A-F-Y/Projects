87bd1fd5a0e64247e5a8998d2b5850f5229c2dcf
Adrian <Adrian1568@live.com>
2025-12-14 21:34:20 -0500

    Update main.py

--- first web/pythonwebsite/pyfiles/main.py
+++ first web/pythonwebsite/pyfiles/main.py
@@ -1,7 +1,7 @@
+# import function from the website package
 from website import create_app
-
+# creates flask app 
 app = create_app()
-
+# only runs the app if the file is executed directly
 if __name__ == '__main__':
-    app.run(debug=True)
-    
+    app.run(debug=True) # starts the flask dev server
